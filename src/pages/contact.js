2ff501f82739665f78ef4b3c666bad82c9c5e942
Luke Bennett <hello@lukebennett.com.au>
2020-10-01 10:25:11 +1000

    Update contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import { Layout, SEO, Hero } from '../components';
+import { Layout, SEO, Hero, ContactSection, GoogleMap } from '../components';
 
 function ContactPage() {
   return (
     <Layout>
       <SEO title="Contact" />
       <HeroSection />
+      <ContactSection />
+      <WhatWeOffer />
+      <GoogleMap />
     </Layout>
   );
 }
@@ -18,6 +21,51 @@ function HeroSection() {
         <h1 className="heading-1">Contact Us Today</h1>
       </div>
     </Hero>
+  );
+}
+
+function WhatWeOffer() {
+  const services = [
+    {
+      label: 'Restorations',
+    },
+    {
+      label: 'New Homes',
+    },
+    {
+      label: 'Commercial <br/>Painting',
+    },
+    {
+      label: 'Concrete Epoxy <br/>Flooring',
+    },
+    {
+      label: 'Rendering',
+    },
+  ];
+
+  return (
+    <article className="relative bg-white">
+      <div className="w-full px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <h2 className="text-center heading-2 text-burnt-orange">
+          More of What We Offer
+        </h2>
+        <div className="grid gap-6 mt-3 md:grid-cols-5">
+          {services.map((service) => (
+            <div className="w-full max-w-sm mx-auto">
+              <div className="relative h-0 aspect-ratio-square">
+                <div className="absolute inset-0 bg-sky-blue">
+                  {/* Image goes here */}
+                </div>
+              </div>
+              <h3
+                dangerouslySetInnerHTML={{ __html: service.label }}
+                className="mt-1 text-sm tracking-wider text-center uppercase"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </article>
   );
 }
 
