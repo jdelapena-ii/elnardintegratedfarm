042897c106a237a5b41b58961c8fc7b5f31cef53
Luke Bennett <hello@lukebennett.com.au>
2020-10-01 10:17:30 +1000

    Bring icons closer together

--- src/components/icons-grid.js
+++ src/components/icons-grid.js
@@ -15,8 +15,8 @@ function IconsGrid() {
     <article className="relative">
       <div className="absolute inset-0">{/* Image goes here */}</div>
       <div className="relative bg-opacity-50 bg-navy">
-        <div className="w-full px-4 py-24 mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <ul className="grid items-center grid-cols-2 gap-6 text-white md:grid-cols-4">
+        <div className="w-full max-w-lg px-4 py-24 mx-auto md:max-w-5xl sm:px-6 lg:px-8">
+          <ul className="grid grid-cols-2 gap-6 text-white md:grid-cols-4">
             {services.map((service) => (
               <li key={nanoid()} className="space-y-6 text-center">
                 <service.icon className="h-24 mx-auto sm:h-32" />
