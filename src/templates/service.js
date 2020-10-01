b0cf48f8a79835de14cab88b34e6e582b49fa383
Luke Bennett <hello@lukebennett.com.au>
2020-10-01 15:54:29 +1000

    Update individual service page

--- src/templates/service.js
+++ src/templates/service.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
+import SanityBlockContent from '@sanity/block-content-to-react';
 import PropTypes from 'prop-types';
 
-import { Layout, SEO, Hero } from '../components';
+import {
+  Layout,
+  SEO,
+  Hero,
+  Service,
+  WhatWeOffer,
+  IconsGrid,
+  ContactSection,
+  GoogleMap,
+} from '../components';
 
 function ServicePageTemplate({ data: { sanityService } }) {
   return (
     <Layout>
       <SEO title={sanityService.title} />
       <HeroSection sanityService={sanityService} />
+      <ServiceSection sanityService={sanityService} />
+      <ServiceImages sanityService={sanityService} />
+      <WhatWeOffer invert />
+      <IconsGrid />
+      <ContactSection />
+      <GoogleMap />
     </Layout>
   );
 }
@@ -35,6 +51,49 @@ function HeroSection({ sanityService }) {
 }
 
 HeroSection.propTypes = {
+  sanityService: PropTypes.object.isRequired,
+};
+
+function ServiceSection({ sanityService }) {
+  return (
+    <Service image={sanityService.mainImage?.asset.fluid} reverse>
+      <Link to={`/services/${sanityService.slug.current}/`}>
+        <h2 className="font-bold heading-2 text-burnt-orange">
+          {sanityService.title}
+        </h2>
+      </Link>
+      <div className="prose">
+        <SanityBlockContent blocks={sanityService._rawBody} />
+      </div>
+    </Service>
+  );
+}
+
+ServiceSection.propTypes = {
+  sanityService: PropTypes.object.isRequired,
+};
+
+function ServiceImages({ sanityService }) {
+  return (
+    <article className="relative bg-white">
+      <div className="w-full px-4 pt-8 pb-20 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div className="grid grid-cols-2 gap-6 md:grid-cols-4">
+          {Array(8)
+            .fill('')
+            .map((image, index) => (
+              <div key={index} className="relative h-0 aspect-ratio-square">
+                <div className="absolute inset-0 bg-sky-blue">
+                  {/* Image goes here */}
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </article>
+  );
+}
+
+ServiceImages.propTypes = {
   sanityService: PropTypes.object.isRequired,
 };
 
